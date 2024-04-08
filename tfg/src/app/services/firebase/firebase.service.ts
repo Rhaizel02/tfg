bd86cfdd01de4b26769b3c7bec3ea0e7eda5a3c2
Bogdan Andrei Szasz <bogs@alumni.uv.es>
2024-04-08 20:12:07 +0200

    Add Firebase configuration to FirebaseService

--- tfg/src/app/services/firebase/firebase.service.ts
+++ tfg/src/app/services/firebase/firebase.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { initializeApp} from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgWRjSWaOOu74VYHLwGIFrMSBH59ZhTAg",
+  authDomain: "rolenroll-95cd8.firebaseapp.com",
+  projectId: "rolenroll-95cd8",
+  storageBucket: "rolenroll-95cd8.appspot.com",
+  messagingSenderId: "839937393400",
+  appId: "1:839937393400:web:ca44e8c60604a543c9f10d",
+  measurementId: "G-1HVKENNFBS"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
+  
   constructor() { }
 }
